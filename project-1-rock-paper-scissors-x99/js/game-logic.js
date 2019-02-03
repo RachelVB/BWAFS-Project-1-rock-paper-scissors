e6e105ae7819ad69fa1fd13f63fd5a38e93d56d8
RachelVB <vanbuskirk2014@hotmail.com>
2019-02-03 14:49:53 -0800

    First commit

---
+++ project-1-rock-paper-scissors-x99/js/game-logic.js
@@ -0,0 +1,32 @@
+// All code should be written in this file.
+
+let playerOneMoveOneType;
+let playerOneMoveTwoType;
+let playerOneMoveThreeType;
+
+let playerOneMoveOneValue;
+let playerOneMoveTwoValue;
+let playerOneMoveThreeValue;
+
+let playerTwoMoveOneType;
+let playerTwoMoveTwoType;
+let playerTwoMoveThreeType;
+
+let playerTwoMoveOneValue;
+let playerTwoMoveTwoValue;
+let playerTwoMoveThreeValue;
+
+
+function setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) {
+  player('player one','player two');
+
+  moveOneType(playerOneMoveOneType, playerTwoMoveOneType) = {
+    playerOneMoveOneType = 'rock',
+    playerTwoMoveOneType = 'rock'
+  }
+
+};
+
+function getRoundWinner() {
+  
+}
