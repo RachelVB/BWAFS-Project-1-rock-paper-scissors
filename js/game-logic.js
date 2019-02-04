619dca7967de39db3af77e3206a8d7893c1d37cb
RachelVB <vanbuskirk2014@hotmail.com>
2019-02-03 17:53:23 -0800

    movetype and value fix

--- js/game-logic.js
+++ js/game-logic.js
@@ -16,14 +16,26 @@ let playerTwoMoveOneValue;
 let playerTwoMoveTwoValue;
 let playerTwoMoveThreeValue;
 
+const playerOne = 'Player One';
+const playerTwo = 'Player Two';
+const tie = 'tie'
 
-function setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) {
-  player('player one','player two');
+const move1Type = 'rock';
+const move2Type = 'paper';
+const move3Type = 'scissors';
 
-  moveOneType(playerOneMoveOneType, playerTwoMoveOneType) = {
-    playerOneMoveOneType = 'rock',
-    playerTwoMoveOneType = 'rock'
+const move1Value = 11;
+const move2Value = 33;
+const move3Value = 55;
+
+function setPlayerMoves (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) {
+  
+  if (!moveOneType || !moveTwoType || !moveThreeType || !moveOneValue || !moveTwoValue || !moveThreeValue) {
+    return;
+    /*Here we are using the ! point to explain that if this '' is NOT 
+    true(not 'true' because we dont have a return value), then clear the 'return'. */
   }
+
 
 };
 
